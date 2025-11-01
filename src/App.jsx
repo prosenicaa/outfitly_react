477622d68b71935a6d37473049b6772dca1025c7
Andjela <andjelaprosenica@gmail.com>
2025-11-01 19:57:52 +0100

    Add skeleton for sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Navbar from './sections/navbar/Navbar';
+import Header from './sections/header/Header';
+import About from './sections/about/About';
+import FAQs from './sections/faqs/FAQs';
+import Contact from './sections/contact/Contact';
+import Footer from './sections/footer/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <FAQs/>
+      <Contact/>
+      <Footer/>
+
+    </main>
   )
 }
 
