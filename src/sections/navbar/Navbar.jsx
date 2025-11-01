3eb5d3dc6d43b82c737805b587eb8dd79ff75703
Andjela <andjelaprosenica@gmail.com>
2025-11-01 23:55:06 +0100

    Add responsive navbar and menu behaviour

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
+import Logo from '../../assets/logo2.png'
+import data from './data'
+import { IoColorPaletteSharp } from 'react-icons/io5'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav__container">
+        <a href="index.html" className='nav__logo'>
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className='nav__menu'>
+          {
+          data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+        <button id='theme__icon'><IoColorPaletteSharp/></button>
+      </div>
+    </nav>
   )
 }
 
