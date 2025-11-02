9d4aa955a4de878f6e7b24121b178e2e01dbfa29
Andjela <andjelaprosenica@gmail.com>
2025-11-02 21:40:44 +0100

    Add header and socials

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,32 @@
+import HeaderImage from '../../assets/header.png'
+import socials from './socials'
 import './header.css'
 
 const Header = () => {
   return (
-    <header id="header"><h2>Header</h2></header>
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__picture">
+          <img src={HeaderImage} alt="Header Picture" />
+        </div>
+        <h3>Outfitly</h3>
+        <p>Outfitly brings together the latest trends, timeless classics, and creative 
+          outfit ideas to help you express your style with confidence. Whether 
+          you're dressing for work, a night out, or casual weekends — find inspiration 
+          that fits you.</p>
+          <div className="header__cta">
+            <a href="#contact" className='btn primary'>Let's talk</a>
+            <a href="#outfits" className='btn light'>Outfits</a>
+          </div>
+          <div className="header__socials">
+            {
+              socials.map(item => <a key={item.id} href={item.link} target="_blank" 
+                rel="noopener noreferrer">{item.icon}</a>
+              )
+            }
+          </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/socials.js
@@ -0,0 +1,13 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
+
+const socials = [
+    {id: 1, link: 'https://www.instagram.com/', icon: <FaInstagram/>},
+    {id: 2, link: 'https://x.com/', icon: <FaXTwitter/>},
+    {id: 3, link: 'https://www.facebook.com/', icon: <FaFacebook/>},
+    {id: 4, link: 'https://www.pinterest.com/', icon: <FaPinterest/>}
+]
+
+export default socials; 
