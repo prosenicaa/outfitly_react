1b54e0bd6c84f0e885a772557d833a8cdfa13de2
Andjela <andjelaprosenica@gmail.com>
2025-11-05 13:33:30 +0100

    Add contact section with email, Messenger and LinkedIn links

---
+++ src/sections/contact/data.js
@@ -0,0 +1,11 @@
+import { IoIosMail } from "react-icons/io";
+import { FaFacebookMessenger } from "react-icons/fa6";
+import { IoLogoLinkedin } from "react-icons/io5";
+
+const data = [
+    {id: 1, icon: <IoIosMail/>, link: 'mailto:andjelaprosenica@gmail.com'},
+    {id: 2, icon: <FaFacebookMessenger/>, link: 'https://www.messenger.com/t/100003621639988'},
+    {id: 3, icon: <IoLogoLinkedin/>, link: 'https://www.linkedin.com/in/anđela-prosenica-22a709269/'}
+]
+
+export default data
